e916963a6c0b69dfc89ac7dd78b896b3376e8dbd
milindjadhav285 <61445321+milindjadhav285@users.noreply.github.com>
2020-05-11 17:22:25 +0530

    Create 2 app.js

---
+++ 2 app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const client = express().listen(port, ()=>console.log('connected to server:'+port));
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const postRoute = require('./routes/posts');
+
+app.use('/posts', postRoute);
+
+app.get('/', (req, res) => {
+    res.send("we are on home");
+});
+
+
+//app.listen(port, () => {
+	//console.log('connected to server: '+port);
+	mongoose.connect(
+		process.env.DB_Connection,{
+    	useNewUrlParser: true,
+    	useUnifiedTopology: true
+    },
+    () => console.log('connected to DB'));
+//});
